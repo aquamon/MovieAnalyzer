f44f23328649e880c1e668fa4684a0dd38a63391
Your Name <you@example.com>
2019-12-09 03:46:48 +0530

    make it better

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from analyzer import app
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    # app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
